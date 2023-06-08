636524e1dad366fd3c7929203259d26e5633e216
S.Mayer <mr.sideway@web.de>
2023-06-08 19:17:46 +0200

    first upload
    car database management tool
    current state without database connection

---
+++ carApp/src/Car.java
@@ -0,0 +1,34 @@
+// Sven Mayer - ITMO2202
+
+public class Car {
+    private String brand, model;
+    private Integer id, hp;
+
+    //  constructor
+    public Car(Integer id, String brand, String model, Integer hp) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.hp = hp;
+    }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+    public String getBrand() { return brand; }
+    public void setBrand(String brand) { this.brand = brand; }
+    public String getModel() { return model; }
+    public void setModel(String model) { this.model = model; }
+    public Integer getHp() { return hp; }
+    public void setHp(Integer hp) { this.hp = hp; }
+
+    public void getCar(){
+        System.out.println("Selected Car: " + "[ID] " + id + " [Brand] " + '"' + brand + '"' + " [Model] " + '"' + model + '"' + " [HP] " + hp);
+    }
+    //    readable output as string
+    @Override
+    public String toString() {
+        return "ID: "  + id + " | " +
+                "Brand: " + brand + " " +
+                "| Model: " + model + " " +
+                "| Horsepower: " + hp;
+    }
+}
